25517bcd5cbdbcd69b9c152f456f51a0c2d94fab
Carefuls <45973246+Carefuls@users.noreply.github.com>
2021-05-13 21:14:04 +0800

    Add files via upload

---
+++ 2-01-TankMovement.cs
@@ -0,0 +1,78 @@
+﻿//2-01-TankMovement
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    public int m_PlayerNumber = 1; //设定玩家变量，通过TankManager调用并传参         
+    public float m_Speed = 12f;  //设定坦克移动速度           
+    public float m_TurnSpeed = 180f;  //设定坦克旋转速度    
+    private string m_MovementAxisName;     //玩家移动键位名称；
+    private string m_TurnAxisName;         //玩家转向键位名称；
+    private Rigidbody m_Rigidbody;         //声明一个私有 rigidbody 刚体组件 m_Rigidbody；
+    private float m_MovementInputValue;    //玩家移动键位输入返回值；
+    private float m_TurnInputValue;        //玩家转向键位输入返回值；
+
+    //脚本加载的时候执行的代码
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();    //将游戏对象 Tank 附加的 rigidbody 刚体组件返回给 m_Rigidbody；
+    }
+
+    //当物体被激活的时候执行的代码 
+    private void OnEnable()
+    {
+        m_Rigidbody.isKinematic = false;            //关闭刚体m_Rigidbody的isKinematic，即刚体m_Rigidbody将受物理的影响；
+        m_MovementInputValue = 0f;                  //初始化m_MovementInputValue；
+        m_TurnInputValue = 0f;                      //初始化m_TurnInputValue；
+    }
+
+    //当物体被停用的时候执行的代码 
+    private void OnDisable()
+    {
+        m_Rigidbody.isKinematic = true;             //启用刚体m_Rigidbody的isKinematic，即刚体m_Rigidbody将不受物理的影响；
+    }
+
+    //场景加载好，开始的时候执行一次 （用来初始化数据），一定会在Update系列函数前面
+    private void Start()
+    {
+        m_MovementAxisName = "Vertical" + m_PlayerNumber;      //为不同玩家设置不同的键位；
+        m_TurnAxisName = "Horizontal" + m_PlayerNumber;
+    }
+
+    // 每一帧执行一次
+    private void Update()
+    {
+        // 存储玩家的键位（以此来匹配引擎声音）
+        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);          //获取玩家移动输入
+        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);   //获取玩家转向输入
+    }
+
+    //固定的刷新率 0.02s 执行一次，主要用来执行物理模拟的脚本
+    private void FixedUpdate()
+    {
+        // Move and turn the tank.
+        Move();
+        Turn();
+    }
+
+    //坦克前后移动函数，根据用户的输入来移动坦克
+    private void Move()
+    {
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+        //创建一个 Vector3 类对象 movement =坦克的位置矢量*玩家移动键位输入返回值*设置的m_Speed*增量时间；
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+        //使用Rigidbody.MovePosition()函数令坦克向(坦克的位置矢量+movement矢量)方向移动；
+    }
+
+    //坦克转向函数
+    private void Turn()
+    {
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+        //创建一个 float 对象 turn = 玩家转向键位输入返回值 * 设置的转向速度m_TurnSpeed*增量时间；
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        //创建一个Quaternion类对象(用于表示旋转的四元数)，接收一个旋转；
+        //Quaternion.Euler (float x, float y, float z)，表示围绕 z 轴旋转 z 度、围绕 x 轴旋转 x 度、围绕 y 轴旋转 y 度；
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+        //使用 Rigidbody.rotation 更改刚体的旋转会在下一个物理模拟步骤后更新变换，以令坦克转向；
+    }
+}
---
+++ 3-01-TankManager.cs
@@ -0,0 +1,50 @@
+﻿//3-01-TankManager  TotalChange 7
+using System;
+using UnityEngine;
+
+[Serializable]
+public class TankManager {
+    public Color m_PlayerColor;            //声明一个公共Color结构m_PlayerColor;
+    public Transform m_SpawnPoint;         //声明一个公共的Transform类对象m_SpawnPoint，储存坦克的出生位置;
+    [HideInInspector] public int m_PlayerNumber;             //玩家编号，使用了该属性的变量不显示；        
+    [HideInInspector] public string m_ColoredPlayerText;
+    [HideInInspector] public GameObject m_Instance;          
+    [HideInInspector] public int m_Wins;
+    private GameObject m_CanvasGameObject;
+    private TankMovement m_Movement; //（3-01），声明一个私有TankMovement脚本组件m_Movement; 
+
+
+    //坦克的初始化设置
+    public void Setup() {
+        m_Movement = m_Instance.GetComponent<TankMovement>(); //（3-02），将坦克的TankMovement脚本组件返回给 m_Movement
+        m_Movement.m_PlayerNumber = m_PlayerNumber; //（3-03），给脚本组件设置玩家编号
+        m_CanvasGameObject = m_Instance.GetComponentInChildren<Canvas>().gameObject;  //获取坦克的Canvas子对象；
+        m_ColoredPlayerText = "<color=#" + ColorUtility.ToHtmlStringRGB(m_PlayerColor) + ">PLAYER " + m_PlayerNumber + "</color>";
+        //设置玩家名称并着色；
+        MeshRenderer[] renderers = m_Instance.GetComponentsInChildren<MeshRenderer>();
+        //创建MeshRenderer对象数组，获取坦克所有MeshRenderer子对象并返回给它；
+        for (int i = 0; i < renderers.Length; i++) {
+            renderers[i].material.color = m_PlayerColor;     //设置坦克的颜色；
+        }
+    }
+
+    //重置坦克
+    public void Reset() {
+        m_Instance.transform.position = m_SpawnPoint.position;     //使坦克回到出生点；
+        m_Instance.transform.rotation = m_SpawnPoint.rotation;     //重置坦克角度；
+        m_Instance.SetActive(false);
+        m_Instance.SetActive(true);
+    }
+
+    //禁用坦克控制
+    public void DisableControl() {
+        m_Movement.enabled = false;              //（3-04），停用m_Movement脚本；
+        m_CanvasGameObject.SetActive(false);    //（3-05），停用坦克的UI显示；
+    }
+
+    //启用坦克控制
+    public void EnableControl() {
+        m_Movement.enabled = true;              //（3-06），启用m_Movement脚本；
+        m_CanvasGameObject.SetActive(true);     //（3-07），启用坦克的UI显示；
+    }
+}
